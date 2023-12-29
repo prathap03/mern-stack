e5ec949d981add5dd47d873be3a897a4c23d02b6
Joe Prathap P J <joeprathappj@gmail.com>
2023-12-29 10:21:21 +0530

    update

--- DAY-001/vite-project/src/SetState.jsx
+++ DAY-001/vite-project/src/SetState.jsx
@@ -2,13 +2,10 @@ import React, { useState } from 'react'
 
 
 export default function SetState() {
-    const [color,setColor]=useState('red-500');
-    const handleChange=()=>{
-        color==='red-500'?setColor('blue-700'):setColor('red-500');
-    }
+    const [color,setColor]=useState(false);
   return (
-    <div className={`flex bg-${color} flex-col items-center justify-center flex-grow `}>
-        <button onClick={()=>handleChange()} className='p-2 text-white bg-yellow-500 rounded-md'>Change Color</button>
+    <div className={`flex ${color? 'bg-red-500': 'bg-blue-500'} flex-col items-center justify-center flex-grow `}>
+        <button onClick={()=>setColor(!color)} className='p-2 text-white bg-yellow-500 rounded-md'>Change Color</button>
     </div>
   )
 }
