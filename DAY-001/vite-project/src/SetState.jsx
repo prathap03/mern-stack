ce5b5f15fe701e52f46f0b9a3b33d7f88cafbc37
Joe Prathap P J <joeprathappj@gmail.com>
2023-12-29 10:23:45 +0530

    color update

--- DAY-001/vite-project/src/SetState.jsx
+++ DAY-001/vite-project/src/SetState.jsx
@@ -5,7 +5,7 @@ export default function SetState() {
     const [color,setColor]=useState(false);
   return (
     <div className={`flex ${color? 'bg-red-500': 'bg-blue-500'} flex-col items-center justify-center flex-grow `}>
-        <button onClick={()=>setColor(!color)} className='p-2 text-white bg-yellow-500 rounded-md'>Change Color</button>
+        <button onClick={()=>setColor(!color)} className='p-2 text-white bg-yellow-500 rounded-md'>Change <span className={`${color? 'text-red-500': 'text-blue-500'} font-bold`}>Color</span></button>
     </div>
   )
 }
