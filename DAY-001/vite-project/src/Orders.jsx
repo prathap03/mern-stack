173e5a71eb75aebb7cb1fb893d5d4d3fc96f3f4e
Joe Prathap P J <joeprathappj@gmail.com>
2024-01-08 21:41:32 +0530

    feature: responsiveness

--- DAY-001/vite-project/src/Orders.jsx
+++ DAY-001/vite-project/src/Orders.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 function Orders({socket}) {
-  const [orders, setOrders] = useState([{ _id: "1" }]);
+  const [orders, setOrders] = useState([]);
   const [isLoading,setLoading] = useState(false)
   useEffect(() => {
     setLoading(true)
@@ -109,16 +109,20 @@ function Orders({socket}) {
       <h1>Orders</h1>
       <div className="flex bg-gradient-to-tr flex-wrap from-blue-500  md:min-h-[12rem]  to-blue-200 p-4 shadow-md rounded-md w-[90%] gap-2 ">
        {isLoading?(
-        <div className="flex items-center justify-center p-1 flex-grow  rounded-[100%]">
+        <div className="flex items-center justify-center p-2 md:p-4 flex-grow  rounded-[100%]">
             <div>
-              <h1>Loading...</h1>
-            </div>
+    <svg aria-hidden="true" class="w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
+</div>
         </div>
        ):  orders.map((order,idx) => {
         return (
           order.status !== "ready" && (
               <div
-              className="bg-white/[40%] shadow-md rounded-md backdrop-blur-md flex-col flex justify-center items-center p-2 md:p-4 min-h-[6rem] text-[0.5rem] transition-all md:text-[1rem] md:min-h-[12rem]"
+              className="bg-white/[40%] shadow-md rounded-md backdrop-blur-md flex-col flex justify-center items-center p-2 md:p-4 min-h-[6rem] text-[0.5rem] transition-all md:text-[1rem] md:min-h-[12rem] "
               key={`${order._id}-${Date.now()}`}
             >
               <h1 key={`${order._id}-${Date.now()}`}>{order._id}</h1>
@@ -141,7 +145,18 @@ function Orders({socket}) {
 
       <h1>Orders - Ready</h1>
       <div className="flex bg-gradient-to-tr flex-wrap from-green-500 to-blue-200 p-4 md:min-h-[12rem] shadow-md rounded-md w-[90%] gap-2 ">
-        {orders.map((order,idx) => {
+      {isLoading?(
+        <div className="flex items-center justify-center p-2 md:p-4 flex-grow  rounded-[100%]">
+            <div>
+    <svg aria-hidden="true" class="w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-white" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
+</div>
+        </div>
+       ):
+        orders.map((order,idx) => {
           return (
             order.status == "ready" && (
               <div
@@ -167,13 +182,14 @@ function Orders({socket}) {
 
       <button onClick={()=>{addOrder()}} className="p-2 mt-5 text-white bg-green-500 rounded-md shadow-md">Add Order</button>
         <div className=" mb-10 mt-10 w-[90%] p-2 gap-2 flex flex-col rounded-md shadow-md bg-gradient-to-tr from-yellow-200 to-slate-200">
+          
           {orders.map((order)=>{
             return(
               <div key={order._id} className="flex items-center gap-4 p-2 bg-white/[40%] rounded-md shadow-md backdrop-blur-md">
                 <h1>${order._id}</h1>
                 <div className="flex justify-end flex-grow gap-2 ">
-                <button onClick={()=>{prepareOrder(order._id)}} disabled={order.status=="ready"?true:false} className="p-2 font-semibold text-white bg-green-500 rounded-full shadow-md disabled:bg-green-300">Ready</button>
-                <button onClick={()=>{deliverOrder(order._id)}} className="p-2 font-semibold text-white bg-red-500 rounded-full shadow-md">Delete</button>
+                <button onClick={()=>{prepareOrder(order._id)}} disabled={order.status=="ready"?true:false} className="p-2 active:scale-90 disabled:active:scale-100 transition-all md:text-[1rem] text-[0.8rem] font-semibold text-white bg-green-500 rounded-full shadow-md disabled:bg-green-300">Ready</button>
+                <button onClick={()=>{deliverOrder(order._id)}} className="p-2 font-semibold text-white bg-red-500 md:text-[1rem] transition-all disabled:active:scale-100 active:scale-90 text-[0.8rem] rounded-full shadow-md">Delete</button>
                 </div>
               </div>
             )
